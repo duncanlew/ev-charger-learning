ee8465fed8db5aacd3ba7f4fd8abda7771c09dec
Duncan Lew <duncanlew@users.noreply.github.com>
2026-08-31 22:42:34 +0200

    added test

--- lambda/availability/src/lib.rs
+++ lambda/availability/src/lib.rs
@@ -12,3 +12,15 @@ pub fn check_availability() -> AvailabilityResponse {
         message: String::from("Charger is mock available"),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reports_mock_availability() {
+        let result = check_availability();
+        assert!(result.available);
+        assert_eq!(result.message, "Charger is mock available");
+    }
+}
